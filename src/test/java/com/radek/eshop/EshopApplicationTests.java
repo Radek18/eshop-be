ae0984c5338d11f9538b6804bf66b457c16fc14d
Radek <>
2024-05-16 21:51:02 +0200

    Initial commit

---
+++ src/test/java/com/radek/eshop/EshopApplicationTests.java
@@ -0,0 +1,12 @@
+package com.radek.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
