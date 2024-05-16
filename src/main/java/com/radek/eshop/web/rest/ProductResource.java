8fe83fcbaff0060188c904b52cebca5e77320ee7
Radek <>
2024-05-16 23:14:44 +0200

    Added Logging

--- src/main/java/com/radek/eshop/web/rest/ProductResource.java
+++ src/main/java/com/radek/eshop/web/rest/ProductResource.java
@@ -2,6 +2,8 @@ package com.radek.eshop.web.rest;
 
 import com.radek.eshop.service.ProductService;
 import com.radek.eshop.service.dto.ProductDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,6 +24,8 @@ import reactor.core.publisher.Mono;
 @Transactional
 public class ProductResource {
 
+    private final Logger log = LoggerFactory.getLogger(ProductResource.class);
+
     private final ProductService productService;
 
     public ProductResource(ProductService productService) {
@@ -30,16 +34,19 @@ public class ProductResource {
 
     @PostMapping("")
     public Mono<ProductDto> createProduct(@RequestBody ProductDto productDto) {
+        log.info("REST request to save Product : {}", productDto);
         return productService.createProduct(productDto);
     }
 
     @GetMapping("")
     public Flux<ProductDto> getAllProducts() {
+        log.info("REST request to get all Products");
         return productService.getAllProducts();
     }
 
     @GetMapping("{productId}")
     public Mono<ProductDto> getProduct(@PathVariable("productId") Long productId) {
+        log.info("REST request to get Product : {}", productId);
         return productService.getProduct(productId);
     }
 
@@ -47,6 +54,7 @@ public class ProductResource {
     public Mono<ProductDto> updateProduct(
             @PathVariable(value = "productId", required = false) final Long productId,
             @RequestBody ProductDto productDto) {
+        log.info("REST request to update Product : {}, {}", productId, productDto);
         return productService.updateProduct(productId, productDto);
     }
 
@@ -54,16 +62,19 @@ public class ProductResource {
     public Mono<ProductDto> partialUpdateProduct(
             @PathVariable(value = "productId", required = false) final Long productId,
             @RequestBody ProductDto productDto) {
+        log.info("REST request to partial update Product partially : {}, {}", productId, productDto);
         return productService.partialUpdateProduct(productId, productDto);
     }
 
     @DeleteMapping("{productId}")
     public Mono<Void> deleteProduct(@PathVariable("productId") Long productId) {
+        log.info("REST request to delete Product : {}", productId);
         return productService.deleteProduct(productId);
     }
 
     @DeleteMapping("")
     public Mono<Void> deleteProductsNotForSale() {
+        log.info("REST request to delete not for sale Products");
         return productService.deleteProductsNotForSale();
     }
 }
