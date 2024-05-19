c24b4843005b191eaca13b824867bf10eb35cf01
Radek <>
2024-05-19 14:38:09 +0200

    Added Liquibase, product table and data

---
+++ src/main/resources/db/data/insert_product.sql
@@ -0,0 +1,11 @@
+INSERT INTO product (product_id, part_no, name, description, is_for_sale, price) VALUES
+(1001, 1001, 'Acer Aspire', 'Didplej 15,6", 4 jádrový procesor, operační paměť 8 GB', true, 7490),
+(1002, 1002, 'Apple MacBook', 'Displej 13,6", 8 jádrový procesor, operační paměť 8 GB', true, 27290),
+(1003, 1003, 'ASUS Vivobook', 'Displej 15,6", 4 jádrový procesor, operační paměť 8 GB', true, 8490),
+(1004, 1004, 'Dell Inspiron', 'Displej 15,6", 4 jádrový procesor, operační paměť 8 GB', true, 9890),
+(1005, 1005, 'Fujitsu LifeBook', 'Displej 13,3", 10 jádrový procesor, operační paměť 16 GB', true, 26990),
+(1006, 1006, 'GIGABITE AORUS', 'Displej 15,6", 12 jádrový procesor, operační paměť 8 GB', true, 29991),
+(1007, 1007, 'HP ProBook', 'Displej 15,6", 10 jádrový procesor, operační paměť 16 GB', true, 18990),
+(1008, 1008, 'Lenovo IdeaPad', 'Displej 15,6", 6 jádrový procesor, operační paměť 16 GB', true, 21499),
+(1009, 1009, 'Microsoft Surface', 'Displej 10,5", 2 jádrový procesor, operační paměť 8 GB', false, 14284),
+(1010, 1010, 'MSI Creator', 'Displej 16", 16 jádrový procesor, operační paměť 64 GB', false, 97990);
